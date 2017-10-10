d84452acb6d0f3d596c2f4175629ba1626319ab9
Augusto Goulart <augusto@goulart.me>
2017-10-10 20:28:37 -0300

    Project upload

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/styles.scss";
+import "normalize-css"
+
+ReactDOM.render(<h1>Boilerplate</h1>, document.getElementById('app'));
